185f91869a0dc01b87db5c63cda1d3d0ed68af5b
Tom Vijlbrief <tvijlbrief@gmail.com>
2015-07-14 22:04:06 +0200

    Depth == 6

--- src/search.cpp
+++ src/search.cpp
@@ -1613,7 +1613,7 @@ void Thread::idle_loop() {
           HistoryStats history;
 
           // Detach history table at low depths
-          if (sp->depth == 7)
+          if (sp->depth == 6)
           {
               std::memcpy(&history, sp->history, sizeof(History));
               sp->history = &history;
