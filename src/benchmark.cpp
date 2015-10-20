a29d60661cafa8c19ea782fae6efbcfa5e48b25a
Tom Vijlbrief <tvijlbrief@gmail.com>
2015-10-20 10:44:15 +0200

    Fix typo

--- src/benchmark.cpp
+++ src/benchmark.cpp
@@ -147,7 +147,6 @@ void benchmark(const Position& current, istream& is) {
   for (size_t i = 0; i < fens.size(); ++i)
   {
       Position pos(fens[i], Options["UCI_Chess960"], Threads.main()->td);
-      fprintf(stdout, "OperN\n");
 
       cerr << "\nPosition: " << i + 1 << '/' << fens.size() << endl;
 
