2667ebd0f02bdaf1b47a361e253e3202d2d2cc0c
Tom Vijlbrief <tvijlbrief@gmail.com>
2015-11-13 13:58:19 +0100

    Only update move when we have none or when we replace the entry
    bench: 8558390

--- src/tt.h
+++ src/tt.h
@@ -43,8 +43,8 @@ struct TTEntry {
 
   void save(Key k, Value v, Bound b, Depth d, Move m, Value ev, uint8_t g) {
 
-    // Preserve any existing move for the same position
-    if (!move16 || (k >> 48) != key16)
+    // Store move if we have none
+    if (!move16)
         move16 = (uint16_t)m;
 
     // Don't overwrite more valuable entries
@@ -53,6 +53,10 @@ struct TTEntry {
      /* || g != (genBound8 & 0xFC) // Matching non-zero keys are already refreshed by probe() */
         || b == BOUND_EXACT)
     {
+        // Preserve any existing move for the same position if we have none
+        if (m || (k >> 48) != key16)
+            move16 = (uint16_t)m;
+
         key16     = (uint16_t)(k >> 48);
         value16   = (int16_t)v;
         eval16    = (int16_t)ev;
