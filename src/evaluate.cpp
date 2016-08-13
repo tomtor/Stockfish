63276049ee2ad88dbf29fe8f37991dbf02d68b9e
Tom Vijlbrief <tvijlbrief@gmail.com>
2016-08-13 09:16:19 +0200

    Simplify and lower midgame bonus
    bench: 7190849

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -654,10 +654,10 @@ namespace {
                 else if (defendedSquares & blockSq)
                     k += 4;
 
-                mbonus += k * rr / 2, ebonus += k * rr;
+                mbonus += k * rr, ebonus += k * rr;
             }
             else if (pos.pieces(Us) & blockSq)
-                mbonus += rr + r * 2, ebonus += rr + r * 2;
+                ebonus += rr + r * 2;
         } // rr != 0
 
         score += make_score(mbonus, ebonus) + PassedFile[file_of(s)];
