8432538d77f5ca47572d8d82e143f0d3fa6343c4
Tom Vijlbrief <tvijlbrief@gmail.com>
2017-11-21 21:51:45 +0100

    9 - 2 * cutNode
    Bench: 5575870

--- src/search.cpp
+++ src/search.cpp
@@ -771,7 +771,7 @@ moves_loop: // When in check search starts from here
                ||(ss-2)->staticEval == VALUE_NONE;
 
     singularExtensionNode =   !rootNode
-                           &&  depth >= (9 - cutNode) * ONE_PLY
+                           &&  depth >= (9 - 2 * cutNode) * ONE_PLY
                            &&  ttMove != MOVE_NONE
                            &&  ttValue != VALUE_NONE
                            && !excludedMove // Recursive singular search is not allowed
