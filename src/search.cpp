f4131a46cf84be489db0484309fd3ddba0309d7b
Tom Vijlbrief <tvijlbrief@gmail.com>
2017-11-21 06:46:13 +0100

    Bench: 5080835

--- src/search.cpp
+++ src/search.cpp
@@ -771,7 +771,7 @@ moves_loop: // When in check search starts from here
                ||(ss-2)->staticEval == VALUE_NONE;
 
     singularExtensionNode =   !rootNode
-                           &&  depth >= 8 * ONE_PLY
+                           &&  depth >= (8 + cutNode) * ONE_PLY
                            &&  ttMove != MOVE_NONE
                            &&  ttValue != VALUE_NONE
                            && !excludedMove // Recursive singular search is not allowed
@@ -830,7 +830,7 @@ moves_loop: // When in check search starts from here
           &&  pos.legal(move))
       {
           Value rBeta = std::max(ttValue - 2 * depth / ONE_PLY, -VALUE_MATE);
-          Depth d = ((depth / (2 * ONE_PLY)) - cutNode) * ONE_PLY;
+          Depth d = (depth / (2 * ONE_PLY)) * ONE_PLY;
           ss->excludedMove = move;
           value = search<NonPV>(pos, ss, rBeta - 1, rBeta, d, cutNode, true);
           ss->excludedMove = MOVE_NONE;
