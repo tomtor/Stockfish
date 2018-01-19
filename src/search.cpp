3de0223a1fc223d4b7a7868dcd81c390a2e17a1b
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-01-19 16:11:10 +0100

    Ready for tuning
    bench: 5263014

--- src/search.cpp
+++ src/search.cpp
@@ -67,7 +67,10 @@ namespace {
   const int skipPhase[] = { 0, 1, 0, 1, 2, 3, 0, 1, 2, 3, 4, 5, 0, 1, 2, 3, 4, 5, 6, 7 };
 
   // Razoring and futility margin based on depth
-  Value razor_margin(Depth d) { return Value(570 + 24 * d / ONE_PLY); }
+  int RMBase= 570;
+  int RMDelta= 32;
+  TUNE(RMBase, RMDelta);
+  Value razor_margin(Depth d) { return Value(RMBase + RMDelta * d / ONE_PLY); }
   Value futility_margin(Depth d) { return Value(150 * d / ONE_PLY); }
 
   // Futility and reductions lookup tables, initialized at startup
