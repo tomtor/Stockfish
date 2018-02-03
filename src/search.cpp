4f8ba896bbe98c9037b813a3dd20e961c30c0b0a
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-02-03 09:53:58 +0100

    Adjust Internal Iterative Deepening parameters
    bench: 4989234

--- src/search.cpp
+++ src/search.cpp
@@ -760,11 +760,11 @@ namespace {
     }
 
     // Step 10. Internal iterative deepening (skipped when in check)
-    if (    depth >= 6 * ONE_PLY
+    if (    depth >= (6 - PvNode) * ONE_PLY
         && !ttMove
         && (PvNode || ss->staticEval + 256 >= beta))
     {
-        Depth d = (3 * depth / (4 * ONE_PLY) - 2) * ONE_PLY;
+        Depth d = std::min(3 * depth / (4 * ONE_PLY) - 2, 2) * ONE_PLY;
         search<NT>(pos, ss, alpha, beta, d, cutNode, true);
 
         tte = TT.probe(posKey, ttHit);
