fc864efbda01ee7c1148979e6cc28e8b3cbc1918
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-05-27 20:44:40 +0200

    Movecount limit depends on material. bench 4631524

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Decrease reduction if opponent's move count is high (~5 Elo)
-              if ((ss-1)->moveCount > 15)
+              if ((ss-1)->moveCount > 13 + pos.count<QUEEN>(WHITE) + pos.count<QUEEN>(BLACK))
                   r -= ONE_PLY;
 
               // Decrease reduction for exact PV nodes (~0 Elo)
