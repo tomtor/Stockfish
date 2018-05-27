4f668604a84e0997cff8fde248626666e1422d49
Tom Vijlbrief <tvijlbrief@gmail.com>
2018-05-27 21:35:17 +0200

    take 4 bench 5192565

--- src/search.cpp
+++ src/search.cpp
@@ -1006,7 +1006,7 @@ moves_loop: // When in check, search starts from here
           else
           {
               // Decrease reduction if opponent's move count is high (~5 Elo)
-              if ((ss-1)->moveCount > 15 + pos.count<QUEEN>(WHITE) + pos.count<QUEEN>(BLACK))
+              if ((ss-1)->moveCount > 15 + pos.count<QUEEN>() / 2)
                   r -= ONE_PLY;
 
               // Decrease reduction for exact PV nodes (~0 Elo)
