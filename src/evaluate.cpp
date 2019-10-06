ef381979fe3eff771817b9884b629ec361d6b4b0
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-10-06 22:18:45 +0200

    bench: 3512483

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -508,7 +508,7 @@ namespace {
     if (defended | weak)
     {
         b = (defended | weak) & (attackedBy[Us][KNIGHT] | attackedBy[Us][BISHOP]);
-        score += make_score(15, 15) * popcount(b & weak & attackedBy[Them][QUEEN]);
+        score += make_score(15, 15) * popcount(b & attackedBy[Them][QUEEN]);
         while (b)
             score += ThreatByMinor[type_of(pos.piece_on(pop_lsb(&b)))];
 
