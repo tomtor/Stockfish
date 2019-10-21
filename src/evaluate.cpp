0b2b36c4134b28d618bcb43b4802ca6876a6c816
Tom Vijlbrief <tvijlbrief@gmail.com>
2019-10-21 19:15:47 +0200

    bench: 4152073

--- src/evaluate.cpp
+++ src/evaluate.cpp
@@ -685,9 +685,9 @@ namespace {
     behind |= shift<Down>(behind);
     behind |= shift<Down+Down>(behind);
 
-    int bonus = popcount(safe) + popcount(behind & safe & ~attackedBy[Them][ALL_PIECES]);
+    int bonus = popcount(safe);
     int weight = pos.count<ALL_PIECES>(Us) - 1;
-    Score score = make_score(bonus * weight * weight / 16, 0);
+    Score score = make_score(bonus * weight * weight / 8, 0);
 
     if (T)
         Trace::add(SPACE, Us, score);
